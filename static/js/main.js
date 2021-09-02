a4d6ad6587481f1aa0c19e29376ef553960de723
designersourav <45048181+designersourav@users.noreply.github.com>
2021-09-02 19:22:48 +0530

    Implemented multiselect dropdown option.

--- static/js/main.js
+++ static/js/main.js
@@ -5,20 +5,22 @@ $('#closeSearch').click(function(){
     $('.searchArea').removeClass('show');
 });
 
-let hrs = "";
-let min = "";
-for (let i = 0; i <= 12; i++) {
-    hrs += "<option>" + i + "</option>";
-    if(i == 12){
-        hrs += "<option selected>" + i + "</option>";
-    }
-}
-for(let j = 0; j < document.getElementsByClassName("hr").length; j++) {
-    document.getElementsByClassName("hr")[j].innerHTML = hrs
-}
-for (let i = 0; i <= 60; i++) {
-    min += "<option>" + i + "</option>";
-}
-for(let j = 0; j < document.getElementsByClassName("min").length; j++) {
-    document.getElementsByClassName("min")[j].innerHTML = min
-}
+document.multiselect('findLocation');
+
+// let hrs = "";
+// let min = "";
+// for (let i = 0; i <= 12; i++) {
+//     hrs += "<option>" + i + "</option>";
+//     if(i == 12){
+//         hrs += "<option selected>" + i + "</option>";
+//     }
+// }
+// for(let j = 0; j < document.getElementsByClassName("hr").length; j++) {
+//     document.getElementsByClassName("hr")[j].innerHTML = hrs
+// }
+// for (let i = 0; i <= 60; i++) {
+//     min += "<option>" + i + "</option>";
+// }
+// for(let j = 0; j < document.getElementsByClassName("min").length; j++) {
+//     document.getElementsByClassName("min")[j].innerHTML = min
+// }
