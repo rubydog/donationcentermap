e55043c05734764c6d80eb5bc03cd975d54942d9
designersourav <45048181+designersourav@users.noreply.github.com>
2021-12-22 21:23:31 +0530

    fresh migrate

--- donationCenter/settings.py
+++ donationCenter/settings.py
@@ -80,22 +80,6 @@ WSGI_APPLICATION = 'donationCenter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.getenv("DB_NAME"),
-#         'USER': os.getenv("DB_USER"),
-#         'PASSWORD': os.getenv("DB_PASSWORD"),
-#         'HOST': os.getenv("DB_HOST"),
-#         'PORT': os.getenv("DB_PORT"),
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -104,6 +88,10 @@ DATABASES = {
         'PASSWORD': 'GiZ#8080',
         'HOST': '89.233.108.250',
         'PORT': '5432',
+    },
+    'sqlite3': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
